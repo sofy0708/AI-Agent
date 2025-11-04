89221a8eb98927dc5e2c293cdb9f498b29801a67
sofy0708 <linsofia07082000@gmail.com>
2025-11-04 19:13:44 +0100

    eliminated hard code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,9 +14,16 @@ if __name__ == "__main__":
 
 client = genai.Client(api_key=api_key)
 
+if len(sys.argv) < 2:
+    print("Error, please provide an input!")
+    sys.exit(1)
+else:
+    content = sys.argv[1]
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=content
 )
+
 print(response.text)
 print("Prompt tokens:", response.usage_metadata.prompt_token_count)
 print("Response tokens:", response.usage_metadata.candidates_token_count)
