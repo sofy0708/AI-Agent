c80a4b10fcdb5059fb97bb131444867453592aa1
sofy0708 <linsofia07082000@gmail.com>
2025-11-04 21:30:40 +0100

    conversation history

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-import sys
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+from google.genai import types
 
 def main():
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
     print("Hello from ai-agent!")
+
+    if len(sys.argv) < 2:
+        print("Idiot! Provide me an input!")
+        sys.exit(1)
+    #else:
+    #    content = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+
+    print(response.text)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 if __name__ == "__main__":
     main()
-
-client = genai.Client(api_key=api_key)
-
-if len(sys.argv) < 2:
-    print("Error, please provide an input!")
-    sys.exit(1)
-else:
-    content = sys.argv[1]
-
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=content
-)
-
-print(response.text)
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
