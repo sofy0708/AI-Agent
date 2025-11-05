500c35ed3d162d5f332601f1dee9531888383f4a
sofy0708 <linsofia07082000@gmail.com>
2025-11-05 15:53:01 +0100

    added --verbose option

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from google.genai import types
 def main():
     load_dotenv()
 
+    user_prompt = " ".join(sys.argv[1:])
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
@@ -15,20 +16,20 @@ def main():
     if len(sys.argv) < 2:
         print("Idiot! Provide me an input!")
         sys.exit(1)
-    #else:
-    #    content = sys.argv[1]
-
+    
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
-    )
+            model='gemini-2.0-flash-001', contents=messages
+        )
 
     print(response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-
+    #print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    #print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
+    if '--verbose' in sys.argv:
+            print(f"User prompt: {user_prompt} \nPrompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}")
 if __name__ == "__main__":
     main()
